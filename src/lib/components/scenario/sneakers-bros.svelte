6dbea2acfe6566d7db9105446563051e9a60d1fd
Hedwy <itshedwy@icloud.com>
2026-08-06 23:07:27 +0200

    add sneakers pictures

--- src/lib/components/scenario/sneakers-bros.svelte
+++ src/lib/components/scenario/sneakers-bros.svelte
@@ -1,1 +1,10 @@
-<img src="scenario/sneakers.png" alt="">
+<script>
+    import ImageGallery from "$lib/components/imageGallery.svelte";
+</script>
+
+<img src="scenario/sneakers_full.webp" alt="">
+
+<ImageGallery pictures={[
+    { src: "scenario/sneakers.png", alt: "" },
+    { src: "scenario/sneakers_2.webp", alt: "" },
+]} />
