895b0c8db0b3720fb0223aa42d41496b6d357e47
Hedwy <itshedwy@icloud.com>
2026-08-06 21:28:41 +0200

    Update wintertale-banner.svelte

--- src/lib/components/scenario/wintertale-banner.svelte
+++ src/lib/components/scenario/wintertale-banner.svelte
@@ -4,6 +4,11 @@
 
 <img src="scenario/wintertale.webp" alt="">
 
+<article>
+    <h1>The world is cold, but there's always warmth in a winter's tale</h1>
+    <p>This illustration was made as a community banner, just one asset among the countless I've created for Wintertale</p>
+</article>
+
 <ImageGallery pictures={[
     { src: "scenario/wintertale_half_1.png", alt: "" },
     { src: "scenario/wintertale_half_2.png", alt: "" },
