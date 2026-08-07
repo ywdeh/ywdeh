66fab54ffac70fa00bfe6330516e6169608fcd25
Hedwy <itshedwy@icloud.com>
2026-08-07 11:26:14 +0200

    Update scenarioDetails.svelte

--- src/lib/components/scenarioDetails.svelte
+++ src/lib/components/scenarioDetails.svelte
@@ -30,28 +30,30 @@
 <svelte:window on:keydown={escape} />
 
 <div class="fixed flex items-center justify-center inset-0 z-90">
-
+    
     <button aria-label="overlay" onclick={onclose} class="fixed inset-0 bg-neutral-950/50 backdrop-saturate-0 backdrop-sepia-10" transition:fade={{ duration: 400 }}></button>
 
-    <div class="fixed bottom-0 w-full max-w-261 h-[90%]" transition:slideY>
-        <div class="flex flex-col w-full h-full bg-neutral-900 rounded-t-xl overflow-y-auto overscroll-contain px-5 pb-10 md:px-10 md:pb-15 font-normal scrollbar-none!">
+    <div class="fixed flex flex-col bottom-0 w-full max-w-261 bg-neutral-900 h-[90%] rounded-t-xl overflow-y-auto px-5 pb-10 md:px-10 md:pb-15 font-normal scrollbar-none! overscroll-contain"
+    transition:slideY>
 
-            <div class="z-80 flex w-full sticky bg-neutral-900/98 backdrop-blur-lg py-10 justify-between -top-1.25 md:top-0">
-                <h1>{title}</h1>
-                <div class="flex items-center gap-5">
-                    <button onclick={onclose} class="group cursor-pointer transition opacity-30 hover:opacity-80 active:opacity-30 -m-2 p-2"><img src="close.svg" alt="" class="size-8"></button>
-                </div>
-                <div class="absolute top-full bg-linear-to-b from-neutral-900/40 to-neutral-900/0 w-full h-10 pointer-events-none"></div>
+        <div class="z-80 flex w-full sticky bg-neutral-900/98 backdrop-blur-lg py-10 justify-between
+                    -top-1.25">
+            <h1>{title}</h1>
+            <div class="flex items-center gap-5">
+                <button onclick={onclose} class="group cursor-pointer transition opacity-30 hover:opacity-80 active:opacity-30 -m-2 p-2"><img src="close.svg" alt="" class="size-8"></button>
             </div>
-
-            <div class="flex flex-col gap-40 pt-5">
-                <div class="flex flex-col gap-20 scenario-body text-lg">
-                    {@render children()}
-                </div>
-                <Footer cta="have a role in mind?" />
-            </div>
-
+            <div class="absolute top-full bg-linear-to-b from-neutral-900/40 to-neutral-900/0 w-full h-10 pointer-events-none"></div>
         </div>
+
+        <div class="flex flex-col gap-40 pt-5">
+
+            <div class="flex flex-col gap-20 scenario-body text-lg">
+                {@render children()}
+            </div>
+
+            <Footer cta="have a role in mind?" />
+        </div>
+
     </div>
 
 </div>
